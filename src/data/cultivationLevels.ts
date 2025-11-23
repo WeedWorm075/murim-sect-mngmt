6f07906e5c597fa71f8e95d2a77f2580e5bcf0d9
WeedWorm075 <weedworm075@gmail.com>
2025-11-23 19:40:11 +0300

    delete project files

--- src/data/cultivationLevels.ts
+++
@@ -1,9 +0,0 @@
-import type { CultivationLevel } from '../types/gameTypes';
-
-export const cultivationLevels: CultivationLevel[] = [
-  { name: "Qi Gathering", maxLevel: 9, progressPerLevel: 100 },
-  { name: "Foundation", maxLevel: 9, progressPerLevel: 150 },
-  { name: "Core Formation", maxLevel: 9, progressPerLevel: 200 },
-  { name: "Nascent Soul", maxLevel: 9, progressPerLevel: 300 },
-  { name: "Spirit Severing", maxLevel: 9, progressPerLevel: 500 }
-];
